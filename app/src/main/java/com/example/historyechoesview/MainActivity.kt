86f0ce2278b0cf0068ff5d0918e690828a7b439c
Giant8east <X2260883456@gmail.com>
2026-03-26 23:55:50 +0800

    首页增加日期选择器以及UI优化

--- app/src/main/java/com/example/historyechoesview/MainActivity.kt
+++ app/src/main/java/com/example/historyechoesview/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.historyechoesview.databinding.ActivityMainBinding
 import com.example.historyechoesview.view.adapter.HistoryEchoesAdapter
 import com.example.historyechoesview.viewmodel.MainViewModel
+import com.google.android.material.datepicker.MaterialDatePicker
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val mainViewModel: MainViewModel by viewModels()
     private val adapter = HistoryEchoesAdapter()
+    private var selectedDateTime = System.currentTimeMillis()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -35,5 +37,24 @@ class MainActivity : AppCompatActivity() {
             Log.i("TAG", "it: ${it}")
             adapter.mData = it
         }
+        binding.tvDate.setOnClickListener {
+            showDatePickerDialog()
+        }
+    }
+
+    fun showDatePickerDialog() {
+        val picker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("选择日期")
+            .setSelection(selectedDateTime)
+            .build()
+        picker.addOnPositiveButtonClickListener {
+
+            val simpleDateFormat = SimpleDateFormat("MM月dd日", Locale.getDefault())
+            val selectDate = simpleDateFormat.format(Date(it))
+            mainViewModel.getHistoryEchoesList(selectDate)
+            binding.tvDate.text = selectDate
+            selectedDateTime = it
+        }
+        picker.show(supportFragmentManager, "DATE_PICKER")
     }
 }
